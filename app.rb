1fffe2ca27d699c48fd9cd28e7f44a2e7e430d01
Chris Didyk <cdidyk@gmail.com>
2012-07-17 14:49:44 -0400

    updates /register to pass along the total and selected courses to the finalize/confirmation page

--- app.rb
+++ app.rb
@@ -6,17 +6,28 @@ require 'sinatra/reloader' if development?
 
 Stripe.api_key = YAML.load_file File.join(File.dirname(__FILE__), "config/stripe.yml")
 
+helpers do
+  def display_money cents
+    "$#{cents/100.0}"
+  end
+end
+
 get "/courses" do
   haml :courses
 end
 
 post "/register" do
-  total = "$#{PriceCalculator.new(params[:courses]).total}"
-  haml :register, locals: {courses: params[:courses], total: total}
+  haml :register, locals: {courses: params[:courses], total: PriceCalculator.new(params[:courses]).total}
 end
 
 post "/finalize" do
   params
+  # charge =
+  #   Stripe::Charge.
+  #     create(amount: 1000,
+  #            currency: "usd",
+  #            card: token,
+  #            description: "payinguser@example.com")
 end
 
 
@@ -27,8 +38,9 @@ class PriceCalculator
     @courses = courses
   end
 
+  #NOTE in cents
   def total
     # discount logic is dealt with here
-    36.95
+    3695
   end
 end
