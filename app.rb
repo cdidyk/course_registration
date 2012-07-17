925c32f2d10aa10ac7b9929a24cae5130249c712
Chris Didyk <cdidyk@gmail.com>
2012-07-17 16:09:07 -0400

    gets payments working with a basic confirmation page (no email yet)

--- app.rb
+++ app.rb
@@ -1,12 +1,16 @@
 require 'sinatra'
 require 'stripe'
 require 'yaml'
+require 'haml'
 
 require 'sinatra/reloader' if development?
 
-Stripe.api_key = YAML.load_file File.join(File.dirname(__FILE__), "config/stripe.yml")
+Stripe.api_key = YAML.load_file(File.join(File.dirname(__FILE__), "config/stripe.yml"))['private_key']
 
 helpers do
+  include Rack::Utils
+  alias_method :h, :escape
+
   def display_money cents
     "$#{cents/100.0}"
   end
@@ -17,17 +21,18 @@ get "/courses" do
 end
 
 post "/register" do
-  haml :register, locals: {courses: params[:courses], total: PriceCalculator.new(params[:courses]).total}
+  courses = params[:courses]
+  haml :register, locals: {courses: courses, total: PriceCalculator.new(courses).total}
 end
 
 post "/finalize" do
-  params
-  # charge =
-  #   Stripe::Charge.
-  #     create(amount: 1000,
-  #            currency: "usd",
-  #            card: token,
-  #            description: "payinguser@example.com")
+  charge =
+    Stripe::Charge.
+      create(amount: params[:total],
+             currency: "usd",
+             card: params[:stripeToken],
+             description: "#{params[:name]}: #{params[:courses]}")
+  haml :confirmation, locals: {name: params[:name], email: params[:email], phone: params[:phone], courses: params[:courses].split(','), total: charge.amount}
 end
 
 
